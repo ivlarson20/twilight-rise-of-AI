33e3655c558ffea4b2e38b524442e306d8c41089
ivlarson20 <ivlarson20@gmail.com>
2025-04-06 12:30:31 -0400

    Initial Commit

---
+++ SDLProject/LevelA.cpp
@@ -0,0 +1,129 @@
+//
+//  LevelA.cpp
+//  SDLProject
+//
+//  Created by isabelle larson on 4/6/25.
+//  Copyright © 2025 ctg. All rights reserved.
+//
+
+#include "LevelA.h"
+#include "Utility.h"
+
+#define LEVEL_WIDTH 14
+#define LEVEL_HEIGHT 8
+
+constexpr char SPRITESHEET_FILEPATH[] = "assets/bella.png";
+constexpr char PLATFROM_FILEPATH[] = "assets/tile.png";
+constexpr char ENEMY1_FILEPATH[] = "assets/vampire1.png";
+constexpr char BACKGROUND_FILEPATH[] = "assets/background1.png";
+
+unsigned int LEVEL_DATA[] =
+{
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    3, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2,
+    3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+};
+
+LevelA::~LevelA(){
+    delete [] m_game_state.enemies;
+    delete m_game_state.player;
+    delete m_game_state.map;
+    delete m_game_state.background;
+    
+    // add music stuff
+}
+
+
+
+void LevelA::initialise(){
+    GLuint map_texture_id = Utility::load_texture("assets/tile.png");
+    m_game_state.map = new Map(LEVEL_WIDTH, LEVEL_HEIGHT, LEVEL_DATA, map_texture_id, 1.0f, 4, 1);
+    
+    GLuint player_texture_id = Utility::load_texture(SPRITESHEET_FILEPATH);
+    
+    int player_walking_animation[4][4] = {
+        { 1, 5, 9, 13 },  // for George to move to the left,
+        { 3, 7, 11, 15 }, // for George to move to the right,
+        { 2, 6, 10, 14 }, // for George to move upwards,
+        { 0, 4, 8, 12 }   // for George to move downwards
+    };
+    
+    glm::vec3 acceleration = glm::vec3(0.0f,-4.905f, 0.0f);
+
+    m_game_state.player = new Entity(
+        player_texture_id,         // texture id
+        5.0f,                      // speed
+        acceleration,              // acceleration
+        3.0f,                      // jumping power
+        player_walking_animation,  // animation index sets
+        0.0f,                      // animation time
+        4,                         // animation frame amount
+        0,                         // current animation index
+        4,                         // animation column amount
+        4,                         // animation row amount
+        0.75f,                      // width
+        1.0f,                       // height
+        PLAYER
+    );
+    
+    m_game_state.player->set_position(glm::vec3(5.0f, 0.0f, 0.0f));
+    
+    m_game_state.player->set_jumping_power(3.0f);
+    
+    // enemies
+    
+    GLuint enemy_texture_id = Utility::load_texture(ENEMY1_FILEPATH);
+    
+    m_game_state.enemies = new Entity[ENEMY_COUNT];
+    
+    for (int i = 0; i < ENEMY_COUNT; i++)
+    {
+    m_game_state.enemies[i] =  Entity(enemy_texture_id, 1.0f, 1.0f, 1.0f, ENEMY, GUARD, IDLE);
+    }
+
+
+    m_game_state.enemies[0].set_position(glm::vec3(8.0f, 0.0f, 0.0f));
+    m_game_state.enemies[0].set_movement(glm::vec3(0.0f));
+    m_game_state.enemies[0].set_acceleration(glm::vec3(0.0f, -9.81f, 0.0f));
+    
+    // background
+//    
+//    GLuint background_texture_id = Utility::load_texture(BACKGROUND_FILEPATH);
+//    m_game_state.background = new Entity();
+//    m_game_state.background->set_texture_id(background_texture_id);
+//
+//    m_game_state.background->set_position(glm::vec3(2.0f,0.0f,0.0f));
+//    m_game_state.background->set_scale(glm::vec3(9.0f, 9.0f, 0.0f));
+    
+    
+    // ADD IN SOUND STUFF
+    
+}
+
+void LevelA::update(float delta_time)
+{
+    m_game_state.player->update(delta_time, m_game_state.player, m_game_state.enemies, ENEMY_COUNT, m_game_state.map);
+    
+    for (int i = 0; i < ENEMY_COUNT; i++)
+    {
+        m_game_state.enemies[i].update(delta_time, m_game_state.player, NULL, NULL, m_game_state.map);
+    }
+}
+
+
+void LevelA::render(ShaderProgram *g_shader_program)
+{
+    m_game_state.map->render(g_shader_program);
+    m_game_state.player->render(g_shader_program);
+    for (int i = 0; i < m_number_of_enemies; i++)
+            m_game_state.enemies[i].render(g_shader_program);
+}
+
+
+
+
---
+++ SDLProject/main.cpp
@@ -0,0 +1,272 @@
+// ADD HEADER HERE
+
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
+#define FIXED_TIMESTEP 0.0166666f
+#define LEVEL1_WIDTH 14
+#define LEVEL1_HEIGHT 8
+#define LEVEL1_LEFT_EDGE 5.0f
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#include <SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+#include "cmath"
+#include <ctime>
+#include <vector>
+#include "Entity.h"
+#include "Map.h"
+#include "Utility.h"
+#include "Scene.h"
+#include "LevelA.h"
+
+
+
+// ––––– CONSTANTS ––––– //
+constexpr int WINDOW_WIDTH  = 640,
+          WINDOW_HEIGHT = 480;
+
+constexpr float BG_RED     = 0.1922f,
+            BG_BLUE    = 0.549f,
+            BG_GREEN   = 0.9059f,
+            BG_OPACITY = 1.0f;
+
+constexpr int VIEWPORT_X = 0,
+          VIEWPORT_Y = 0,
+          VIEWPORT_WIDTH  = WINDOW_WIDTH,
+          VIEWPORT_HEIGHT = WINDOW_HEIGHT;
+
+constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
+           F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
+constexpr char BACKGROUND_FILEPATH[] = "assets/background1.jpg";
+
+
+constexpr float MILLISECONDS_IN_SECOND = 1000.0;
+constexpr glm::vec3 INIT_SCALE_BACK = glm::vec3(10.0f, 5.0f, 0.0f);
+
+
+
+// ––––– GLOBAL VARIABLES ––––– //
+Scene *g_current_scene;
+LevelA *g_level_a;
+
+SDL_Window* g_display_window;
+bool g_game_is_running = true;
+
+ShaderProgram g_shader_program;
+glm::mat4 g_view_matrix, g_projection_matrix, g_background_matrix;
+
+float g_previous_ticks = 0.0f;
+float g_accumulator = 0.0f;
+GLuint g_background_texture_id;
+
+
+// ––––– GENERAL FUNCTIONS ––––– //
+void switch_to_scene(Scene *scene){
+    g_current_scene = scene;
+    g_current_scene->initialise();
+}
+
+
+void initialise()
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    g_display_window = SDL_CreateWindow("Twilight rise of the AI",
+                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                      WINDOW_WIDTH, WINDOW_HEIGHT,
+                                      SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
+    SDL_GL_MakeCurrent(g_display_window, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    // ––––– VIDEO ––––– //
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    g_view_matrix = glm::mat4(1.0f);
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+
+    glUseProgram(g_shader_program.get_program_id());
+
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+    
+    g_background_texture_id = Utility::load_texture(BACKGROUND_FILEPATH);
+    
+    // level a
+    g_level_a = new LevelA();
+    switch_to_scene(g_level_a);
+    
+    
+    
+
+    // ––––– GENERAL ––––– //
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void process_input()
+{
+    g_current_scene->get_state().player->set_movement(glm::vec3(0.0f));
+    
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        // ————— KEYSTROKES ————— //
+        switch (event.type) {
+            // ————— END GAME ————— //
+            case SDL_QUIT:
+            case SDL_WINDOWEVENT_CLOSE:
+                g_game_is_running = false;
+                break;
+                
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_q:
+                        // Quit the game with a keystroke
+                        g_game_is_running = false;
+                        break;
+                        
+                    case SDLK_SPACE:
+                        // ————— JUMPING ————— //
+                        if (g_current_scene->get_state().player->get_collided_bottom())
+                        {
+                            g_current_scene->get_state().player->jump();
+                            Mix_PlayChannel(-1,  g_current_scene->get_state().jump_sfx, 0);
+                        }
+                         break;
+                        
+                    default:
+                        break;
+                }
+                
+            default:
+                break;
+        }
+    }
+    
+    // ————— KEY HOLD ————— //
+    const Uint8 *key_state = SDL_GetKeyboardState(NULL);
+
+    if (key_state[SDL_SCANCODE_LEFT])        g_current_scene->get_state().player->move_left();
+    else if (key_state[SDL_SCANCODE_RIGHT])  g_current_scene->get_state().player->move_right();
+     
+    if (glm::length( g_current_scene->get_state().player->get_movement()) > 1.0f)
+        g_current_scene->get_state().player->normalise_movement();
+ 
+}
+
+
+void update()
+{
+    float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
+    float delta_time = ticks - g_previous_ticks;
+    g_previous_ticks = ticks;
+
+    delta_time += g_accumulator;
+    
+    g_background_matrix = glm::mat4(1.0f);
+    g_background_matrix = glm::scale(g_background_matrix, INIT_SCALE_BACK);
+
+    if (delta_time < FIXED_TIMESTEP)
+    {
+        g_accumulator = delta_time;
+        return;
+    }
+ 
+
+    while (delta_time >= FIXED_TIMESTEP)
+    {
+        g_current_scene->update(FIXED_TIMESTEP);
+        delta_time -= FIXED_TIMESTEP;
+    }
+
+    g_accumulator = delta_time;
+    
+    g_view_matrix = glm::mat4(1.0f);
+    
+    if (g_current_scene->get_state().player->get_position().x > LEVEL1_LEFT_EDGE) {
+        g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-g_current_scene->get_state().player->get_position().x, 3.75, 0));
+    } else {
+        g_view_matrix = glm::translate(g_view_matrix, glm::vec3(-5, 3.75, 0));
+    }
+}
+
+void draw_object(glm::mat4 object_g_model_matrix, GLuint object_texture_id)
+{
+    // Vertices
+    float vertices[] = {
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
+    };
+
+    // Textures
+    float texture_coordinates[] = {
+        0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+    
+    g_shader_program.set_model_matrix(object_g_model_matrix);
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+    
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    // We disable two attribute arrays now
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+}
+
+void render()
+{
+    g_shader_program.set_view_matrix(g_view_matrix);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    draw_object(g_background_matrix, g_background_texture_id);
+
+    
+
+    g_current_scene->render(&g_shader_program);
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown()
+{
+    SDL_Quit();
+
+    delete g_level_a;
+}
+
+// ––––– GAME LOOP ––––– //
+int main(int argc, char* argv[])
+{
+    initialise();
+
+    while (g_game_is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+    shutdown();
+    return 0;
+}
